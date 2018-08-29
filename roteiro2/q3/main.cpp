5568204d15fe576e6e728dcfe53b99d591926ddd
hanavitor <vitorhanael@hotmail.com>
2018-08-29 13:21:29 -0300

    q3

---
+++ roteiro2/q3/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "funcionario.h"
+#include "consultor.h"
+
+using namespace std;
+
+int main(){
+
+    Funcionario funcionario("2014300032","Pedro",2300);
+    Consultor consultor("20139499921","Carlos",2300);
+
+    cout << funcionario.getSalario() << endl;
+    cout << funcionario.getNome() << endl;
+    cout << funcionario.getMatricula() << endl;
+
+    cout << consultor.getSalario(0.1) << endl;
+    cout << consultor.getNome() << endl;
+    cout << consultor.getMatricula() << endl;
+
+    return 0;
+}
